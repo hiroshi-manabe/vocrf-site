eab069d4dd52fbdb7b86822d02c49be347396a35
Hiroshi Manabe <t_takeda25@hotmail.com>
2011-07-23 14:43:08 +0900

    Commented out the content of ImeLog().

--- test/ime.js
+++ test/ime.js
@@ -427,11 +427,13 @@ function AjaxIME(doc) {
   }
 
   function ImeLog(result) {
+    /*
     if (ImeRawInput_ == '') ImeRawInput_ = result;
     if (ImeSelectedIndex_ >= 1) ImeCache_[ImeRawInput_] = result;
     var request = "?" + encodeURI(ImeRawInput_);
     ImeJsonpLog_ = new JSONRequest(ImeCGI_ + request);
     ++ImeID_;
+    */
   }
 
   function ImeRequestCallback(result) {
