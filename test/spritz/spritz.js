1dc6e87e8ab61aeeb87b64221daec3a572ff0093
Hiroshi Manabe <manabe.hiroshi@gmail.com>
2018-07-04 23:45:15 +0900

    Updated.

--- test/spritz/spritz.js
+++ test/spritz/spritz.js
@@ -62,7 +62,7 @@ function spritzify(input, output, wpm, no_preprocess){
 function pivot(word){
     var length = word.length;
 
-    if(word.charCodeAt(0) >= 0x80) {
+    if(!word.match(/^[\u{0}-\u{7ff}\W]+$/) {
         result = "<span class='start'>" + word + "</span>";
     }
     else if(length<14){
@@ -114,7 +114,7 @@ function pivot(word){
 
     }
 
-    result = result.replace(/\./g, "<span class='invisible'>.</span>");
+    result = result.replace(/\./g, "<span class='invisible'>.</span>").replace(/•/g, ".");
 
     return result;
 }
